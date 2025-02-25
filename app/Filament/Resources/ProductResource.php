0b04d83c28b0cb39e8af4c974d2098f13ef4546a
rjusm <rustam190401@mail.ru>
2025-02-25 18:15:10 +0500

    add ProductCategoryResource

--- app/Filament/Resources/ProductResource.php
+++ app/Filament/Resources/ProductResource.php
@@ -18,7 +18,9 @@ class ProductResource extends Resource
 {
     protected static ?string $model = Product::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-cube';
+
+    protected static ?string $navigationGroup = 'Product Management';
 
     public static function form(Form $form): Form
     {
